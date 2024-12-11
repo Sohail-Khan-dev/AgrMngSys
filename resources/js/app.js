81cc571ba7613a8bc60c425766199f0e0ae708e6
sohail-khan <sohail8338@gmail.com>
2024-12-11 22:11:42 +0500

    fix the inde.html page successfully

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
+import GLightbox from "glightbox";
+import "glightbox/dist/css/glightbox.min.css";
+
+const lightbox = GLightbox();
+
+import Swiper from "swiper";
+import "swiper/css";
+
+const swiper = new Swiper(".swiper-container", {
+    // Add your Swiper configuration here
+});
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index.html', function () {
+    return view('index');
+});
+Route::get('/about.html', function () {
+    return view('about');
+});
+Route::get('/blog-details.html', function () {
+    return view('blog-details');
+});
+Route::get('/blog.html', function () {
+    return view('blog');
+});
+Route::get('/contact.html', function () {
+    return view('contact');
+});
+Route::get('/portfolio-details.html', function () {
+    return view('portfolio-details');
+});
+Route::get('/service-details.html', function () {
+    return view('service-details');
+});
+Route::get('/starter-page.html', function () {
+    return view('starter-page');
+});
+Route::get('/team.html', function () {
+    return view('team');
+});
+
