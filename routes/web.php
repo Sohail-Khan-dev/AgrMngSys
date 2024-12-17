f88715d086e868db8a05bcedfd56c0bdc68c2eac
sohail-khan <sohail8338@gmail.com>
2024-12-17 20:50:43 +0500

    set the index page in the according to the Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/index.html', function () {
+Route::get('/index', function () {
     return view('index');
 });
 Route::get('/about.html', function () {
