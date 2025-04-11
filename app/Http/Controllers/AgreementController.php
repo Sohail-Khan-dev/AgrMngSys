376cea85a86a0c7f1a68c5b552918fc3bf1e72a2
sohail-khan <sohail8338@gmail.com>
2025-04-11 15:21:38 +0500

    dubug

--- app/Http/Controllers/AgreementController.php
+++ app/Http/Controllers/AgreementController.php
@@ -57,6 +57,7 @@ class AgreementController extends Controller
     }
     public function getAllAgreements(Request $request)
     {
+         dd($request->all());
         $agreements = Agreement::where('id', $request->id)->get();
         return response()->json(['agreements' => $agreements], 200);
     }
